d54e8200130ca5d9e4308c25536ef4ddcd2892bb
IyanSR <ikhyan7@gmail.com>
2023-01-01 21:29:46 +0800

    feat: init tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 
+import '../styles/main.css'
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
